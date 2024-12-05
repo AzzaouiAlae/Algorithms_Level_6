68fd8ccbb7e9dc4ed1c7514012c95717e1f27f89
Alae azzaoui <azzaoui@outlook.com>
2024-12-05 22:25:14 +0100

    linear and binary search

---
+++ 001_Linear_Search/Linear_Search/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Linear_Search;
+
+class Program
+{
+    static int linear_Search(int[] arr, int numToFind)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == numToFind)
+                return i;
+        }
+        return -1;
+    }
+    static void print_result_of_Search(int numToFind, int result)
+    {
+        if (result != -1)
+            Console.WriteLine($"Number {numToFind} found in index {result}");
+        else
+            Console.WriteLine($"Number {numToFind} not found");
+    }
+    static void Main(string[] args)
+    {
+        int [] arr = {64, 34, 25, 12, 22, 11, 90, 0};
+        int numToFind = 0;
+        int result = linear_Search(arr, numToFind);
+
+        print_result_of_Search(numToFind, result);
+    }
+}
---
+++ 002_Binary_Search/Binary_Search/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Binary_Search;
+
+class Program
+{
+    static int binasry_search(int []arr, int value)
+    {
+        int i = 0;
+        int start = 0, end = arr.Length, middle;
+        while(start != end && start < end)
+        {
+            middle = ((end - start) / 2) + start;
+            if(arr[middle] == value)
+                return middle;
+            if(arr[middle] > value)
+                end = middle;
+            if(arr[middle] < value)
+                start = middle;
+            i++;
+            Console.WriteLine(i.ToString());
+        } 
+        return -1;
+    } 
+    static void print_result_of_Search(int value, int result)
+    {
+        if (result != -1)
+            Console.WriteLine($"Number {value} found in index {result}");
+        else
+            Console.WriteLine($"Number {value} not found");
+    }
+    static void Main(string[] args)
+    {
+        int []arr = {22,25,37,41,45,46,49,51,55,58,70,80,82,90,95};
+        int value = 45;
+        int result = binasry_search(arr, value);
+
+        print_result_of_Search(value, result);
+    }
+}
